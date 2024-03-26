d03606e41169357f929b4b6d39bec716342f25a4
Ricardo Salvatorelli <rsca4321@gmail.com>
2024-03-25 21:46:52 -0400

    [Routing] Define routers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,3 +1,13 @@
 import { Router } from 'express'
+import { userRouter } from './user.routes'
+import { authRouter } from './auth.routes'
+import { productRouter } from './product.routes'
+import { orderRouter } from './order.routes'
 
 export const router = Router()
+
+// Routes
+router.use('/auth', authRouter)
+router.use('/users', userRouter)
+router.use('/products', productRouter)
+router.use('/orders', orderRouter)
