948e69b71cb9b2569b4d0477637a7386516eb129
Ricardo Salvatorelli <rsca4321@gmail.com>
2024-03-26 00:26:55 -0400

    Applies middleware to routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,11 +3,19 @@ import { userRouter } from './user.routes'
 import { authRouter } from './auth.routes'
 import { productRouter } from './product.routes'
 import { orderRouter } from './order.routes'
+import { resultMiddleware } from '../middlewares/result.middleware'
+import { requestBodyMiddleware } from '../middlewares/request-body.middleware'
 
 export const router = Router()
+
+// Before route middlewares
+router.use(requestBodyMiddleware)
 
 // Routes
 router.use('/auth', authRouter)
 router.use('/users', userRouter)
 router.use('/products', productRouter)
 router.use('/orders', orderRouter)
+
+// After route middlewares
+router.use(resultMiddleware)
