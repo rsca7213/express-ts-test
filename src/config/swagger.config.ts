72232f11d483af46a6bf637f0ffe774b78d3af8b
Ricardo Salvatorelli <rsca4321@gmail.com>
2024-03-26 22:03:53 -0400

    Add console log to indicate swagger is mounted

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -4,4 +4,5 @@ import { Application } from 'express'
 
 export function setupSwagger(app: Application): void {
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  console.log('[Swagger] Now running on /docs route')
 }
